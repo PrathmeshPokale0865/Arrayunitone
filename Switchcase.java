2112a8215d023b6f80e30114cfbc7f60320b8c64
PrathmeshPokale0865 <prathmesh.pokale24@pcu.edu.in>
2025-01-06 13:04:47 +0530

    Update Switchcase.java

--- Switchcase.java
+++ Switchcase.java
@@ -1,14 +1,23 @@
-import org.w3c.dom.ls.LSOutput;
 
 import java.util.Scanner;
 
 public class Switchcase
 {
     public static void main(String[] args) {
+        
+        //array is declared as the daysofweek
         int daysofweek;
+        
+        //it is used for taking input
         Scanner scan=new Scanner(System.in);
+
+        //to print the days of week
         System.out.println("days of week");
+        
+         //this gives enter no to the output
         daysofweek=scan.nextInt();
+
+        //here the cases are build 
         switch(daysofweek){
             case 1:
                 System.out.println("sunday");
