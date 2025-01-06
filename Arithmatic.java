0f447dd7f9089eea10b4d94d8528f8ee197c0f79
PrathmeshPokale0865 <prathmesh.pokale24@pcu.edu.in>
2025-01-06 11:36:01 +0530

    Update Arithmatic.java

--- Arithmatic.java
+++ Arithmatic.java
@@ -11,20 +11,21 @@ public class Arithmatic {
         int f = a * b;
         //% for remindar
         int g = a % b;
-
+    //used for printing the operations 
         System.out.println(c);//11
         System.out.println(d);//3
         System.out.println(e);//1
         System.out.println(f);//28
         System.out.println(g);//3
 
-        //increment operator
+        //increment operator and decrement operator
 
         int x = 10, y = 10;
         int p, q;
 
         p = x++;
         q = ++y;
+    //used for printing the p and q operator
         System.out.println("p is" + p + "x is" + x);
         System.out.println("q is" + q + "y is" + y);
     }
