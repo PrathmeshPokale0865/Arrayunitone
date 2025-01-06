dc49dfab434a486f1cc24b87d5bcba9c4744d66b
PrathmeshPokale0865 <prathmesh.pokale24@pcu.edu.in>
2025-01-06 11:41:03 +0530

    Update P04.java

--- P04.java
+++ P04.java
@@ -1,14 +1,25 @@
-import java.sql.SQLOutput;
+//used for taking the input
 import java.util.Scanner;
 
 public class P04
 {
     public static void main(String[] args) {
+       
+        //here area and radius is declared
         double area,r;
+        
+        //the scanner system is used for to read the input from user
         Scanner scan=new Scanner(System.in);
+        
+        //it shows the output as enter the radius
         System.out.println("enter radius");
+        
+        //stores the value of area and radius 
         r=scan.nextDouble();
-area=3.14159*r*r;
+        
+        //the value is entered here of area
+         area=3.14159*r*r;
+        //it prints the area
         System.out.println("Area is %f");
 }
 }
