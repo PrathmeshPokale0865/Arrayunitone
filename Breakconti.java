4bccf9449a5eac06ad07ae5abdc765e1391befa3
PrathmeshPokale0865 <prathmesh.pokale24@pcu.edu.in>
2025-01-06 12:52:50 +0530

    Update Breakconti.java

--- Breakconti.java
+++ Breakconti.java
@@ -1,13 +1,22 @@
 public class Breakconti {
     public static void main(String[] args) {
+        
+        //variable is declared
         int i=1;
+
+        //to do multiple statements 
         while(i<10){
-            System.out.println(1);
+            
+        //to print the no
+          System.out.println(1);
+
+        //breaks the satatment means when 5 comes the program ends
             if (i == 5) {
                 break;
 
-
             }
+
+            //after each loop the operator is increment 
             i++;
             continue;
         }
